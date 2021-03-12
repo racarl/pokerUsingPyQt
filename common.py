da12870b055e584c060c56c840748b2ba5945c63
CarlMariavanWeber <CarlMariavanWeber@gmail.com>
2021-03-12 21:49:37 +0900

    update

--- common.py
+++ common.py
@@ -5,6 +5,6 @@ import variables from other .py
 https://hashcode.co.kr/questions/3362/%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%97%90%EC%84%9C-%EB%8B%A4%EB%A5%B8-%ED%8C%8C%EC%9D%BC%EC%9D%98-%EC%A0%84%EC%97%AD%EB%B3%80%EC%88%98%EB%A5%BC-%EA%B3%B5%EC%9C%A0%ED%95%98%EA%B3%A0-%EC%8B%B6%EC%8A%B5%EB%8B%88%EB%8B%A4
 """
 
-tuple0_imageFileDir = ('cardPNG',)
+tuple0_imageFileDir = ('card_image_DIR',)
 tuple_number = ('A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K')
 tuple_pattern = ('S', 'D', 'H', 'C')
--- download_card_image_from_wikimedia.py
+++ download_card_image_from_wikimedia.py
@@ -6,6 +6,9 @@ https://soyoung-new-challenge.tistory.com/92
 
 BeautifulSoup
 https://velog.io/@neulhan/%EC%B4%88%EB%B3%B4%EB%8F%84-%ED%95%A0-%EC%88%98-%EC%9E%88%EB%8A%94-python%EC%9C%BC%EB%A1%9C-%EB%84%A4%EC%9D%B4%EB%B2%84%EC%97%90%EC%84%9C-%EC%8B%A4%EC%8B%9C%EA%B0%84-%EA%B2%80%EC%83%89%EC%96%B4-%EC%A0%95%EB%B3%B4-%EA%B0%80%EC%A0%B8%EC%98%A4%EA%B8%B0-2-BeautifulSoup-1uk4asqet0
+
+check a file exists
+https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions
 """
 import requests
 from bs4 import BeautifulSoup as bs
@@ -22,19 +25,19 @@ def get_card_url(number, pattern):
     return url
 
 
-def curl_url_fileName(url, fileName):
-    """
+def download_card_image(whether_overwrite=False):
+    if not os.path.isdir(tuple0_imageFileDir[0]):
+        os.system('mkdir ' + tuple0_imageFileDir[0])
 
-    :rtype: None
-    """
-    os.system('curl ' + url + ' > ' + fileName)
-    return
+    for pattern in tuple_pattern:
+        for number in tuple_number:
+            file_name = './' + str(tuple0_imageFileDir[0]) + '/' + str(number) + str(pattern) + '.png'
+            if os.path.isfile(file_name) and not whether_overwrite:
+                pass
+            else:
+                url = get_card_url(number, pattern)
+                os.system('curl ' + url + ' > ' + file_name)
 
 
 if __name__ == '__main__':
-    os.system('mkdir ' + tuple0_imageFileDir[0])
-    for pattern in tuple_pattern:
-        for number in tuple_number:
-            url = get_card_url(number, pattern)
-            fileName = './' + str(tuple0_imageFileDir[0]) + '/' + str(number) + str(pattern) + '.png'
-            curl_url_fileName(url, fileName)
+    download_card_image()
